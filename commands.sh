0cc4568435a2dd0535fa9343bf58a1fea7c39aec
Herman.B <Herman.B@zdf.de>
2024-06-20 14:58:47 +0200

    add comments to commands.sh

--- commands.sh
+++ commands.sh
@@ -1,6 +1,10 @@
+# installation/deployment commands
 kubectl apply -f k8s/
+# command for checking the logs of the backapp
 kubectl get pods
 kubectl logs backapp-deployment-b6c946688-6ksdr backapp
+# command for listing all frontapp related resources (1 command)
 kubectl get all -l homework=frontapp
+# command for listing deleting both applications’ resources (1 command)
 kubectl delete -f k8s/
             
