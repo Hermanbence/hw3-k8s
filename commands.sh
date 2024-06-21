e08176f8bf666bafa0d46db89e505bbdcd1c5042
Herman.B <Herman.B@zdf.de>
2024-06-21 10:32:59 +0200

    changed commands

--- commands.sh
+++ commands.sh
@@ -1,17 +1,13 @@
 # installation/deployment commands
 kubectl apply -f k8s/
-# kubectl apply -f backapp-deployment.yaml -f backapp-service.yaml -f frontapp-deployment.yaml -f frontapp-service.yaml -f frontapp-ingress.yaml
-# Required to use the -f flag foreach yaml
 
 # command for checking the logs of the backapp
-kubectl get pods
-# kubectl logs pod-name container-name
-kubectl logs backapp-deployment-b6c946688-6ksdr backapp
+kubectl logs deployment/backapp
 
 # command for listing all frontapp related resources (1 command)
 kubectl get all -l homework=frontapp
 
 # command for listing deleting both applications’ resources (1 command)
 kubectl delete pod,service,ingress --all
-# kubectl delete all --all
-# Unfortunetly it's not deleting the kind: ingress   
+# kubectl delete all --all -n my-other-namespace
+# Unfortunetly it's not deleting the kind: ingress  
--- my_note.md
+++ my_note.md
@@ -14,6 +14,10 @@ kubectl apply -f deployment.yaml
 - f
 ``` 
 file name
+
+kubectl apply -f backapp-deployment.yaml -f backapp-service.yaml -f frontapp-deployment.yaml -f frontapp-service.yaml -f frontapp-ingress.yaml
+- Required to use the -f flag foreach yaml
+
 kubectl apply -f k8s/
 - apply a folder it run all the yaml in it.
 - even ingress
@@ -35,3 +39,10 @@ kubectl describe pod/frontapp-deployment-56fcf56dc9-jkgnt
 
 ## kubectl logs
 kubectl logs pod-name container-name
+kubectl logs deployment/backapp
+- name of the kind/name of the deployment
+deployment.yaml
+```
+metadata:
+  name: backapp
+```
